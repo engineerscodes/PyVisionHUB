a1e707afb304e0072d55eb84eeb67ffae8cd92c0
engineerscodes <practicalengineers05@gmail.com>
2021-01-26 01:20:39 +0530

    printing lesson

---
+++ 01.Basic/Printing.py
@@ -0,0 +1,50 @@
+
+#Double quotes and single quotes
+print("Naveen")
+
+#print('Naveen'n' car') error
+
+#fix 1
+
+print("Naveen 's car")
+
+#or fix2
+print('Naveen \'s car')
+#output-Naveen's car back lash is ued to fix the error
+
+#display raw string
+
+print('c:\Doc\naveen')
+'''output : willbe not be c:/Doc/naveen
+output willbe c:/Doc
+aveen .Because of \n
+to fix it use r or display raw strinf
+'''
+
+print(r'c:\Doc\naveen')
+
+
+#print N times
+print( 10*"Rhea\n")
+'''
+OUTPUT ---<>
+
+Naveen
+Naveen 's car
+Naveen 's car
+c:\Doc
+aveen
+c:\Doc\naveen
+Rhea
+Rhea
+Rhea
+Rhea
+Rhea
+Rhea
+Rhea
+Rhea
+Rhea
+Rhea
+
+'''
+
