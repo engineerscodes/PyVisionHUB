6e8d4d0d55e6b77bde9ffab7a43c0eeca515f5d3
engineerscodes <practicalengineers05@gmail.com>
2021-02-13 19:54:01 +0530

    while

---
+++ 01.Basic/Loops/WhileLoop.py
@@ -0,0 +1,18 @@
+i=1
+while i <=15:
+    print(i ,end=" ")
+    i=i+1
+print("\nDone Stop!!!!!!!!!!!!!!!!!!!!!!!!!")
+
+i=1
+while i<=6 :
+    print("*"*i)
+    i=i+1
+print("Done ")
+
+i=1
+while i<=6 :
+    print(" "*i,end="")
+    print("*"*(6-i))
+    i=i+1
+print("Done ")
