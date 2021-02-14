b98addca613f8f137880b9ad80ac6c514685708d
engineerscodes <practicalengineers05@gmail.com>
2021-02-14 13:14:08 +0530

    Exceptions

--- Fuctions/basics.py
+++ Fuctions/basics.py
@@ -1,12 +1,12 @@
 
 
 
-def name (name) :
+def name (name) : # Parameter
     print(name)
 
 
 
-name("cdrcrecrcrcrc")
+name("cdrcrecrcrcrc") # arguments
 name(r"c\naveen")
 name(15)
 
@@ -22,3 +22,45 @@ print(floor(15.9999))
   #sin 45=
 #print(sin(45/180*pi))
 
+
+#Keyword Argument
+def callme(firstname,lastname) :
+    print(f" HI {firstname } {lastname} ")
+
+callme(lastname="m",firstname="Naveen")
+
+def callme2(firstname,lastname="JAMES") :
+    print(f" HI {firstname } {lastname} ")
+
+callme2(firstname="Naveen")
+
+def callme3(firstname,lastname="Jammy wooo") :
+    print(f" HI {firstname } {lastname} ")
+
+#callme3(lastname="m","Naveen") #position argument must be before keyword arguments
+callme3("Virat",lastname="Kohli") ##position argument must be before keyword arguments
+
+print(callme3("Virat",lastname="Kohli"))  #by default all fuction return none
+#return statement
+
+def retSquare(num) :  #by default all fuction return none
+    return num *num
+
+print(retSquare(5))
+
+sqr=retSquare(9)
+print(sqr)
+
+
+def retMultipleV (n,n2):
+    return [n,n2] #return a list
+
+lists=retMultipleV(100,"Naveen")
+print(type(lists))
+
+
+def retMultipleVTuple (n,n2):
+    return n,n2 #return a Tuple
+
+lists=retMultipleVTuple(100,"Naveen")
+print(type(lists))
