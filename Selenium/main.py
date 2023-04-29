44352f765948f2fc8585fa28be5837defbab2b16
NAVEEN <68312849+engineerscodes@users.noreply.github.com>
2023-04-29 20:33:57 +0530

    added des folder

--- Selenium/main.py
+++ Selenium/main.py
@@ -2,5 +2,6 @@ from get_ss_dev import Webscraper
 
 driver = Webscraper(chrome_driver_path="C:\\Users\\NAVEEN\Documents\\chromedriver.exe",user_data="C:\\Users\\NAVEEN\\AppData\\Local\\Google\\Chrome\\User Data")
 driver.load_page("https://twitter.com/home", 60)
-driver.screenshot(filename="Tbl", custom_width=1600)
+driver.screenshot(filename="Tbl",des_folder="C:\\Users\\NAVEEN\\Pictures\\Saved Pictures",custom_width=1600)
 driver.close()
+
