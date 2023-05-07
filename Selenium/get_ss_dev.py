cd5d1669d053a35e8d493528a5d8d5bab9b4e8fb
NAVEEN <68312849+engineerscodes@users.noreply.github.com>
2023-05-07 17:23:41 +0530

    Add files via upload

--- Selenium/get_ss_dev.py
+++ Selenium/get_ss_dev.py
@@ -1,3 +1,4 @@
+import glob
 import os
 import time
 
@@ -10,14 +11,25 @@ from datetime import date
 class Webscraper:
     __driver: WebDriver
     __title: str
+    __download_path: str
 
-    def __init__(self, chrome_driver_path="C:\\Users\\NAVEEN\Documents\\chromedriver.exe",user_data="C:\\Users\\NAVEEN\\AppData\\Local\\Google\\Chrome\\User Data"):
+    def __init__(self, chrome_driver_path="C:\\Users\\NAVEEN\Documents\\chromedriver.exe",
+                 user_data="C:\\Users\\NAVEEN\\AppData\\Local\\Google\\Chrome\\User Data",
+                 download_path: str = os.path.dirname(__file__)):
+        self.__download_path = download_path
         self.__chrome_options = webdriver.ChromeOptions()
         self.__chrome_options.add_argument('--headless')
+        # prefs = {"download.default_directory": self.__download_path}
+        # self.__chrome_options.add_experimental_option("prefs", prefs)
         self.__chrome_options.add_argument('--disable-gpu')
         self.__chrome_options.add_argument('debuggerAddress=127.0.0.1:9222')
         self.__chrome_options.add_argument(f'--user-data-dir={user_data}')
+        self.__chrome_options.add_argument(f'--download.default_directory={self.__download_path}')
+        self.__chrome_options.add_argument(
+            "--user-agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.53 Safari/537.36'")
         self.__driver = webdriver.Chrome(chrome_driver_path, chrome_options=self.__chrome_options)
+        self.__driver.execute_cdp_cmd('Page.setDownloadBehavior',
+                                      {'behavior': 'allow', 'downloadPath': download_path})
 
     def load_page(self, url: str = "https://github.com/engineerscodes", loading_time: int = 30):
         print("Reaching to Endpoint ........................")
@@ -35,7 +47,21 @@ class Webscraper:
         today = date.today().strftime("%d_%m_%Y")
         self.__driver.find_element(By.TAG_NAME, 'body').screenshot(f"{des_folder}\\{filename}_{today}.png")
         print(f'File Saved -> check path {des_folder}\\{filename}_{today}.png')
+        time.sleep(10)  # wait till file is downloaded
+        self.__file_handler()
 
     def close(self):
         self.__driver.close()
         print("closing program")
+
+    def __file_handler(self):
+        today = date.today().strftime("%d_%m_%Y")
+        extension = ".png"  # Filter by file Type
+        os.chdir(self.__download_path)
+        files = glob.glob("*" + extension)
+        files.sort(key=os.path.getmtime, reverse=True)
+        most_recent_file = os.path.join(self.__download_path, files[0])
+        file_name, file_ext = os.path.splitext(most_recent_file)
+        new_rename_path = f"{file_name}_{today}{file_ext}"
+        os.rename(most_recent_file, new_rename_path)
+        print(f"Most recently downloaded file: {most_recent_file} has been renamed to -> {new_rename_path}")
--- Selenium/main.py
+++ Selenium/main.py
@@ -1,7 +1,9 @@
 from get_ss_dev import Webscraper
 
-driver = Webscraper(chrome_driver_path="C:\\Users\\NAVEEN\Documents\\chromedriver.exe",user_data="C:\\Users\\NAVEEN\\AppData\\Local\\Google\\Chrome\\User Data")
-driver.load_page("https://twitter.com/home", 60)
-driver.screenshot(filename="Tbl",des_folder="C:\\Users\\NAVEEN\\Pictures\\Saved Pictures",custom_width=1600)
-driver.close()
+driver = Webscraper(chrome_driver_path="C:\\Users\\NAVEEN\Documents\\chromedriver.exe",
+                    user_data="C:\\Users\\NAVEEN\\AppData\\Local\\Google\\Chrome\\User Data",
+                    download_path="C:\\Users\\NAVEEN\\Pictures\\Saved Pictures")
+driver.load_page("https://github.com/engineerscodes/Khonshu", 20)
 
+driver.screenshot(filename="Tbl",custom_width=1600)
+#driver.close()
